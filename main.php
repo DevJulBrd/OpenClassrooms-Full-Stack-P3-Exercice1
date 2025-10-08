c79ee601bc768dc0c80cde8641289e80270935fd
Julien Bernard <julienbernard@Juliens-MacBook-Pro.local>
2025-10-08 19:07:14 +0200

    'list' Add to Loop

--- main.php
+++ main.php
@@ -1,14 +1,38 @@
 <?php 
+require __DIR__ . '/DBConnect.php';
+require __DIR__ . '/Contact.php';
+require __DIR__ . '/ContactManager.php';
+
+// First connection to the database
+$db  = new DBConnect('127.0.0.1', 3306, 'P3-exercice1-CLI', 'root', '', 'utf8mb4');
+$pdo = $db->getPDO();
+
+// Contact manager instance
+$manager = new ContactManager($pdo);
+
+// Command loop
 while (true) {
     $line = readline("Entrer votre commande : ");
-    echo "Vous avez saisi : $line\n";
-    
-    // Listening commands
-    $command = strtolower(trim($line));
+    if ($line === false) { echo PHP_EOL; break; } // Ctrl+D
+    $cmd = strtolower(trim($line));
 
-    // Unsers commands
-    if ($command === "list") {
-        echo "Affichage de la liste des contactes\n";
+    if ($cmd === 'list') {
+        $contacts = $manager->findAll();
+
+        if (empty($contacts)) {
+            echo "Le carnet est vide.\n";
+        } else {
+            foreach ($contacts as $contact) {
+                echo $contact->toString(), PHP_EOL;
+            }
+        }
         continue;
     }
+
+    if ($cmd === 'quit') {
+        echo "Au revoir\n";
+        break;
+    }
+
+    echo "Commande inconnue: '$cmd'\n";
 }
